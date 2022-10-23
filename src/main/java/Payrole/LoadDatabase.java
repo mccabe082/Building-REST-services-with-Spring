4c3d85315dc337fdfb912197eed94747e83f7fcc
David McCabe <david.mccabe082@gmail.com>
2022-10-23 15:48:34 +0100

    add load database method

--- src/main/java/Payrole/LoadDatabase.java
+++ src/main/java/Payrole/LoadDatabase.java
@@ -6,17 +6,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // contains beans
 class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
-
+    @Bean // produce a bean for IOC container
+    CommandLineRunner initDatabase(EmployeeRepository repository)
+    {
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+
+    @Bean
+    CommandLineRunner append2Database(EmployeeRepository repository)
+    {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("David McCabe", "engineer")));
         };
     }
 }
