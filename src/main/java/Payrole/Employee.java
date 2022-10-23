a8878ab4d3ae78029b968bdf7ea03b5af5984316
David McCabe <david.mccabe082@gmail.com>
2022-10-23 15:06:09 +0100

    rationalise Employee class

--- src/main/java/Payrole/Employee.java
+++ src/main/java/Payrole/Employee.java
@@ -4,64 +4,48 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+@Entity  // make this object ready for storage in a JPA-based data store
 class Employee {
 
-    private @Id @GeneratedValue Long id;
+    private @Id @GeneratedValue Long id;  // primary key and automatically populated by the JPA provider.
     private String name;
     private String role;
 
     public Employee() {}
 
-    public Employee(String name, String role) {
-
+    public Employee(String name, String role)
+    {
         this.name = name;
         this.role = role;
     }
 
-    public Long getId() {
-        return this.id;
-    }
+    public Long getId() { return this.id; }
+    public void setId(Long id) { this.id = id; }
 
-    public String getName() {
-        return this.name;
-    }
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getRole() {
-        return this.role;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    public String getRole() { return this.role; }
+    public void setRole(String role) { this.role = role; }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
+        // check if object is the same
+        if (this == o) return true;
 
-        if (this == o)
-            return true;
-        if (!(o instanceof Employee))
-            return false;
+        // check if type is the same
+        if (!(o instanceof Employee)) return false;
+
+        // check if employee object is equivalent
         Employee employee = (Employee) o;
         return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
                 && Objects.equals(this.role, employee.role);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
-    }
+    public int hashCode() { return Objects.hash(this.id, this.name, this.role); }
 
     @Override
-    public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
-    }
+    public String toString() { return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}'; }
 }
